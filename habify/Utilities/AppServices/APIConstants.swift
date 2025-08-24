778a52b39e32454ac635ae8c1f5fd15238693cef
Wahyu-Sedana <wsstudiocode@gmail.com>
2025-08-24 12:04:37 +0800

    feat: change logo icon app and create app service to handle api

---
+++ habify/Utilities/AppServices/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  habify
+//
+//  Created by I Putu Wahyu Eka Putra Sedana on 24/08/25.
+//
+
+import Foundation
+
+struct APIConstans {
+    static let baseURL = "http://127.0.0.1:3000/api/v1"
+    static let habitsEndpoint = "\(baseURL)/habits"
+    static let usersEndpoint = "\(baseURL)/users"
+}
